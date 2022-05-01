950c529654ca2f4f732c8e64d39f8bf4a5493e13
litmus-zhang <40471908+litmus-zhang@users.noreply.github.com>
2022-05-01 13:29:25 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const advice = document.querySelector('.advice');
+const dice = document.querySelector('.dice');
+const id = document.querySelector('.id');
+
+
+const url = 'https://api.adviceslip.com/advice';
+
+var text,adviceID;
+const getAdvice = () =>
+{ 
+    fetch(url).then(response => response.json()).then(data =>
+    { 
+         text = data.slip.advice;
+        adviceID = data.slip.id;
+        id.textContent = adviceID;
+        advice.textContent = text;
+
+       
+
+    });
+    
+}
+
+dice.addEventListener('click', getAdvice);
+
+setInterval(getAdvice, 5000);
